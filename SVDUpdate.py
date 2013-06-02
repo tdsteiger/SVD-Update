111aa1ebecec03370bb46095a24b901c100b342f
Taylor Steiger <tsteiger@uw.edu>
2013-06-02 00:35:41 -0600

    Update SVDUpdate.py

--- SVDUpdate.py
+++ SVDUpdate.py
@@ -1,36 +1,39 @@
 import numpy as np
 
-mat = np.array([[1,2,3],[3,2,5],[5,3,1],[7,7,7]])
-U, s, V = np.linalg.svd(mat, full_matrices = False)
+def svd_update(U, S, V, X, c = None, add = False, down = False):
+    V = np.vstack([V, np.zeros(V.shape[0])])
+    if down or type(c) == type(np.array([])):
+        b = np.zeros(V.shape[0])
+        b[-1] = 1
+        b = np.reshape(b, (b.shape[0], 1))
+        if down:
+            a = np.multiply(X[:,0], -1)
+        elif add:
+            a = c
+        else:
+            a = X[:,0] - c
+    else:
+        b = np.zeros(V.shape[0])
+        b = np.add(b, 1)
+        #still need to make a for this case... this is the case where you just want to recenter
 
-V = np.vstack([V, np.zeros(V.shape[0])])
-S = np.diag(s)
-a = np.array([1,2,4,4])
-a = np.reshape(a, (a.shape[0], 1))
-b = np.zeros(V.shape[0])
-b[-1] = 1
-b = np.reshape(b, (b.shape[0], 1))
+    m = np.dot(np.transpose(U), a)
+    p = a - np.dot(U, m)
+    Ra = np.linalg.norm(p)
+    P = np.multiply((1 / Ra), p)
+    n = np.dot(np.transpose(V), b)
+    q = b - np.dot(V, n)
+    Rb = np.linalg.norm(q)
+    Q = np.multiply((1 / Rb), q)
 
-m = np.dot(np.transpose(U), a)
-p = a - np.dot(U, m)
-Ra = np.linalg.norm(p)
-P = np.multiply((1 / Ra), p)
-n = np.dot(np.transpose(V), b)
-q = b - np.dot(V, n)
-Rb = np.linalg.norm(q)
-Q = np.multiply((1 / Rb), q)
+    k = np.vstack([S, np.zeros(S.shape[0])])
+    K = np.zeros((k.shape[0], k.shape[0]))
+    K[:,:-1] = k
+    t = np.transpose(np.vstack([m, Ra]))
+    stack = np.vstack([n, Rb])
+    dot = np.dot(stack, t)
+    K = np.add(K, dot)
 
-k = np.vstack([S, np.zeros(S.shape[0])])
-K = np.zeros((k.shape[0], k.shape[0]))
-K[:,:-1] = k
-t = np.transpose(np.vstack([m, Ra]))
-stack = np.vstack([n, Rb])
-c = np.dot(stack, t)
-K = np.add(K, c)
+    D, P = matrix(K).eigenmatrix_right()
 
-D, P = matrix(K).eigenmatrix_right()
-
-#results
-Uprime = (P.inverse()).transpose()
-Vprime = P
-Sprime = D
+    return ((P.inverse()).transpose(), D, P)
