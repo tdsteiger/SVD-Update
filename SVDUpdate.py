ed12ddc4e4fd33483c4af1dabb4847131e3d0eac
Taylor Steiger <tsteiger@uw.edu>
2013-06-10 14:07:38 -0600

    Update SVDUpdate.py

--- SVDUpdate.py
+++ SVDUpdate.py
@@ -7,25 +7,25 @@ def svd_update(U, S, V, X, c = None, add = False, down = False):
         b[-1] = 1
         b = np.reshape(b, (b.shape[0], 1))
         if down:
-            a = np.multiply(X[:,-1], -1)
+            a = np.reshape(np.multiply(X[:,-1], -1), (-1, 1))
         elif add:
-            a = c
+            a = np.reshape(c, (-1, 1))
         else:
-            a = X[:,-1] - c
+            a = np.reshape(X[:,-1] - c, (-1, 1))
     else:
         ones = np.zeros(V.shape[0])
         ones = np.add(b, 1)
-        b = np.reshape(ones, (ones.shape[0], 1))
-        a = np.multiply((-1/X.shape[1]), np.dot(X, b))
+        b = np.reshape(ones, (-1, 1))
+        a = np.reshape(np.multiply((-1/X.shape[1]), np.dot(X, b)), (-1, 1))
 
-    m = np.dot(np.transpose(U), a)
-    p = a - np.dot(U, m)
+    m = np.reshape(np.dot(np.transpose(U), a), (-1, 1))
+    p = np.reshape(a - np.dot(U, m), (-1, 1))
     Ra = np.linalg.norm(p)
-    P = np.multiply((1 / Ra), p)
-    n = np.dot(np.transpose(V), b)
+    P = np.reshape(np.multiply((1 / Ra), p), (-1, 1))
+    n = np.reshape(np.dot(np.transpose(V), b), (-1, 1))
     q = b - np.dot(V, n)
     Rb = np.linalg.norm(q)
-    Q = np.multiply((1 / Rb), q)
+    Q = np.reshape(np.multiply((1 / Rb), q), (-1, 1))
 
     k = S
     K = np.zeros((k.shape[0] + 1, k.shape[0] + 1))
@@ -33,11 +33,8 @@ def svd_update(U, S, V, X, c = None, add = False, down = False):
     stack = np.vstack(np.append(m, Ra))
     t = np.reshape(np.append(n, Rb), (1, -1))
     dot = np.dot(stack, t)
-    print K
-    print dot
     K = np.add(K, dot)
-    print K
 
-    D, P = matrix(K).eigenmatrix_right()
+    D, P = np.linalg.eig(K)
 
-    return ((P.inverse()).transpose(), D, P)
+    return (np.transpose(np.linalg.inv(P)), D, P)
